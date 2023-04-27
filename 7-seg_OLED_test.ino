3fe9ffe89d3ccc7d8e48e898073fee284fe2da5b
NHGM <94941257+nhashimoto-gm@users.noreply.github.com>
2023-04-27 22:05:26 +0900

    Update 7-seg_OLED_test.ino

--- 7-seg_OLED_test.ino
+++ 7-seg_OLED_test.ino
@@ -28,25 +28,15 @@ int value = 0;
 void setup() {
   Serial.begin(9600);
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000); // Pause for 2 seconds
-
-  // Clear the buffer
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.clearDisplay();
+  display.display();
 }
 
 void drawchar(int value, int cycles) {
   display.clearDisplay();
 
-  display.setTextSize(4);      // Normal 1:1 pixel scale
+  display.setTextSize(3.5);      // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(1, 1);     // Start at top-left corner
 
